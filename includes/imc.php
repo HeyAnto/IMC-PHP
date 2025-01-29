913855ac5d4d0bace135e8ff2243a2c94643ebc5
HeyAnto <antonin63170@gmail.com>
2025-01-29 12:03:22 +0100

    Update IMC

--- includes/imc.php
+++ includes/imc.php
@@ -13,19 +13,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     } else {
         $tailleEnMètres = $taille / 100; // Converti la taille cm => mètres
         $imc = $poids / ($tailleEnMètres * $tailleEnMètres); // Poids divisé par la taille au carré
-    }
 
-    if ($imc < 18.5) { // IMC inférieur à 18.5 = la catégorie
-        $categorie = "Insuffisance pondérale (maigreur)";
-    } elseif ($imc < 25) { // IMC compris entre 18.5 et 25 = la catégorie
-        $categorie = "Corpulence normale";
-    } elseif ($imc < 30) {
-        $categorie = "Surpoids";
-    } elseif ($imc < 35) {
-        $categorie = "Obésité modérée";
-    } elseif ($imc < 40) {
-        $categorie = "Obésité sévère";
-    } else { // IMC est supérieur ou égal à 40 = la catégorie
-        $categorie = "Obésité morbide ou massive";
+        if ($imc < 18.5) { // IMC inférieur à 18.5 = la catégorie
+            $categorie = "Insuffisance pondérale (maigreur)";
+        } elseif ($imc < 25) { // IMC compris entre 18.5 et 25 = la catégorie
+            $categorie = "Corpulence normale";
+        } elseif ($imc < 30) {
+            $categorie = "Surpoids";
+        } elseif ($imc < 35) {
+            $categorie = "Obésité modérée";
+        } elseif ($imc < 40) {
+            $categorie = "Obésité sévère";
+        } else { // IMC est supérieur ou égal à 40 = la catégorie
+            $categorie = "Obésité morbide ou massive";
+        }
     }
 }
--- index.php
+++ index.php
@@ -41,14 +41,14 @@
 
                 <?php if (isset($erreur)): ?>
                     <div class="card-danger flex flex-column flex-center">
-                        <?= $erreur ?>
+                        <?php echo $erreur ?>
                     </div>
                 <?php endif; ?>
 
                 <?php if (isset($imc)): ?>
                     <div class="card-success flex flex-column">
-                        <p>Votre IMC est de : <b><?= number_format($imc, 1) ?></b></p>
-                        <p>Catégorie : <b><?= $categorie ?></b></p>
+                        <p>Votre IMC est de : <b><?php echo number_format($imc, 1) ?></b></p>
+                        <p>Catégorie : <b><?php echo $categorie ?></b></p>
                     </div>
                 <?php endif; ?>
             </div>
